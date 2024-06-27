37c1ecb7778922fc17d9bc1975ae4b8b2a92f29e
Гамид Халидов <khalidov-gamid@mail.ru>
2024-06-27 15:41:33 +0300

    Initial Commit

---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Gamıd Khalıdov on 27.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
