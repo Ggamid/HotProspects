719bf9d5efdc43ca9b9a322f5f9574d224a8ccbd
Гамид Халидов <khalidov-gamid@mail.ru>
2024-06-29 21:59:26 +0300

    generating a qrcode

--- HotProspects/MeView.swift
+++ HotProspects/MeView.swift
@@ -4,12 +4,45 @@
 //
 //  Created by Gamıd Khalıdov on 28.06.2024.
 //
-
+import CoreImage.CIFilterBuiltins
 import SwiftUI
 
 struct MeView: View {
+    
+    @AppStorage("name") private var name = "Anonymous"
+    @AppStorage("emailAddress") private var emailAddress = "you@yoursite.com"
+    
+    var context = CIContext()
+    var filter = CIFilter.qrCodeGenerator()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Form {
+            TextField("Name", text: $name)
+                .font(.title)
+                .textContentType(.name)
+            
+            TextField("Email", text: $emailAddress)
+                .font(.title2)
+                .textContentType(.emailAddress)
+            
+            Image(uiImage: generateQRCode(frome: "\(name)\n\(emailAddress)"))
+                .interpolation(.none)
+                .resizable()
+                .scaledToFill()
+        }
+    }
+    
+    
+    func generateQRCode(frome string: String) -> UIImage {
+        filter.message = Data(string.utf8)
+        
+        if let outputImage = filter.outputImage {
+            if let cgImage = context.createCGImage(outputImage, from: outputImage.extent) {
+                return UIImage(cgImage: cgImage)
+            }
+        }
+        
+        return UIImage(systemName: "xmark.circle") ?? UIImage()
     }
 }
 
